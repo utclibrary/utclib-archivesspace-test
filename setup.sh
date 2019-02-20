e044d1ffd6c344e1fc7aa6157f90f9ea961e201b
Steven D. Shelton <Steven-Shelton@UTC.edu>
2019-02-20 10:16:19 -0500

    added as as a service

---
+++ setup.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+# Update packages
+echo "Running yum update..."
+yum -y update
+echo "Installing epel ..."
+yum -y install epel-release yum-utils
+echo "Installing misc ..."
+yum -y install vim curl unzip java-1.8.0-openjdk
+#yum-config-manager --enable remi-php72
+#yum -y install php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysql
+#echo "Installing Apache ..."
+#yum -y install httpd
+#echo "Start Apache"
+#systemctl start httpd
+echo "Installing MariaDB ..."
+yum -y install mariadb-server
+systemctl start mariadb
+systemctl status mariadb
+# check to see if archivesspace database exist - if not create and import
+if ! mysql -u root -e 'use archivesspace'; then
+mysql -u root -e "create database archivesspace default character set utf8"
+#mysql -u root archivesspace < /vagrant/utc.sql;
+mysql -u root -e  "grant all on archivesspace.* to 'as'@'localhost' identified by 'as123'"
+mysql -u root archivesspace < /vagrant/utc.sql
+echo "Setting up database"
+/opt/archivesspace/scripts/setup-database.sh
+fi
+#echo "Check for utc.sql.gz"
+#gz_dump="/vagrant/utc.sql.gz"
+#if [ -f "$gz_dump" ]
+#then
+	#printf "Extracting gz file...\n";
+#        gunzip -k /vagrant/*.gz
+#else
+	#printf "No DB.gz file found.\n";
+#fi
+
+#echo "Check for UTC db"
+#db_dump="/vagrant/utc.sql"
+#if [ -f "$db_dump" ]
+#then
+	#printf "Loading DB...\n";
+        #mysql --user=as --password=as123 archivesspace < /vagrant/utc.sql
+
+#else
+	#printf "No DB file found.\n";
+#fi
+# create systemd for archivesspace
+cp -u /vagrant/archivesspace.service /etc/systemd/system/archivesspace.service
+systemctl enable archivesspace.service
+systemctl start archivesspace.service
+systemctl status archivesspace.service
+
+
+#echo "Adding ArchivesSpace to system startup"
+#cd /etc/init.d
+#ln -s /aspace/archivesspace/archivesspace.sh archivesspace
+
+#update-rc.d archivesspace defaults
+#update-rc.d archivesspace enable
+
+
+# echo "Starting ArchivesSpace"
+# /opt/archivesspace/archivesspace.sh start
+#setenforce 0
+echo "USE"
+echo "http://192.168.33.11:8089/ – the backend"
+echo "http://192.168.33.11:8080/ – the staff interface"
+echo "http://192.168.33.11:8081/ – the public interface"
+echo "http://192.168.33.11:8082/ – the OAI-PMH server"
+echo "http://192.168.33.11:8090/ – the Solr admin console"
